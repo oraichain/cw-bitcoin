df5fb4b91d9894a0020776f5f8f7a7119c838b0e
Pham Tu <tubackkhoa@gmail.com>
2024-07-12 19:29:53 +0700

    using bitcoin-wasm

--- wasm/src/helpers.rs
+++ wasm/src/helpers.rs
@@ -1,8 +1,7 @@
 use crate::error::ContractResult;
-use crate::interface::PartialMerkleTree;
 use bitcoin::{
     consensus::{Decodable, Encodable},
-    util::uint::Uint256,
+    util::merkleblock::PartialMerkleTree,
     BlockHeader, Script, Transaction,
 };
 use wasm_bindgen::prelude::*;
@@ -11,20 +10,18 @@ use wasm_bindgen::prelude::*;
 macro_rules! encode_ops {
     ($inner:ty) => {
         ::paste::paste! {
-
             #[wasm_bindgen]
-            pub fn [<toBinary $inner>] (value: JsValue) -> ContractResult<String> {
-                    let inner: $inner = serde_wasm_bindgen::from_value(value)?;
+            pub fn [<toBinary $inner>] (inner: $inner) -> ContractResult<String> {
                     let mut dest: Vec<u8> = Vec::new();
                     inner.consensus_encode(&mut dest)?;
                     Ok(base64::encode(dest))
             }
 
             #[wasm_bindgen]
-            pub fn [<fromBinary $inner>] (value: &str) -> ContractResult<JsValue> {
+            pub fn [<fromBinary $inner>] (value: &str) -> ContractResult<$inner> {
                     let slice = base64::decode(value)?;
                     let inner: $inner = Decodable::consensus_decode(&mut slice.as_slice())?;
-                    Ok(serde_wasm_bindgen::to_value(&inner)?)
+                    Ok(inner)
             }
         }
     };
@@ -32,6 +29,5 @@ macro_rules! encode_ops {
 
 encode_ops!(BlockHeader);
 encode_ops!(Script);
-encode_ops!(Uint256);
-encode_ops!(Transaction);
 encode_ops!(PartialMerkleTree);
+encode_ops!(Transaction);
