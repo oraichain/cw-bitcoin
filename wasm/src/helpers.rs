31ab6c04203c63ea191d0ec8afc2f9c56a4d44ed
Pham Tu <tubackkhoa@gmail.com>
2024-08-12 18:03:24 +0700

    add buffer method

--- wasm/src/helpers.rs
+++ wasm/src/helpers.rs
@@ -23,6 +23,24 @@ macro_rules! encode_ops {
                     let inner: $inner = Decodable::consensus_decode(&mut slice.as_slice())?;
                     Ok(inner)
             }
+
+            #[wasm_bindgen]
+            pub fn [<toBuffer $inner>] (inner: $inner) -> ContractResult<js_sys::Uint8Array> {
+                    let mut dest: Vec<u8> = Vec::new();
+                    inner.consensus_encode(&mut dest)?;
+                    let buffer = js_sys::Uint8Array::new_with_length(dest.len() as u32);
+                    buffer.copy_from(&dest);
+                    Ok(buffer)
+            }
+
+            #[wasm_bindgen]
+            pub fn [<fromBuffer $inner>] (value: js_sys::Uint8Array) -> ContractResult<$inner> {
+                    let slice = base64::decode(value.to_vec().as_slice())?;
+                    let inner: $inner = Decodable::consensus_decode(&mut slice.as_slice())?;
+                    Ok(inner)
+            }
+
+
         }
     };
 }
