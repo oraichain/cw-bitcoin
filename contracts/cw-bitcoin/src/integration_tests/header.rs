d595d048dc94698af5299284f5462360f000587d
perfogic <longdieu12x@gmail.com>
2024-08-27 15:26:25 +0700

    add test header

--- contracts/cw-bitcoin/src/integration_tests/header.rs
+++ contracts/cw-bitcoin/src/integration_tests/header.rs
@@ -15,6 +15,98 @@ where
 }
 
 #[test]
+fn test_relay_bulk_headers() {
+    let (mut app, accounts) = MockApp::new(&[("perfogic", &coins(100_000_000_000, "orai"))]);
+    let owner = Addr::unchecked(&accounts[0]);
+    let token_factory_addr = app.create_tokenfactory(owner.clone()).unwrap();
+    let bitcoin_bridge_addr = app
+        .create_bridge(
+            owner.clone(),
+            &msg::InstantiateMsg {
+                token_factory_addr: token_factory_addr.clone(),
+                relayer_fee: Uint128::from(0 as u16),
+                relayer_fee_receiver: Addr::unchecked("relayer_fee_receiver"),
+                relayer_fee_token: AssetInfo::NativeToken {
+                    denom: "orai".to_string(),
+                },
+                token_fee_receiver: Addr::unchecked("token_fee_receiver"),
+                swap_router_contract: None,
+                osor_entry_point_contract: None,
+            },
+        )
+        .unwrap();
+
+    let mut conf = Conf::default();
+    conf.p2p = P2P::Yes;
+    let node = BitcoinD::with_conf(bitcoind::downloaded_exe_path().unwrap(), &conf).unwrap();
+    let alice_address = node.client.get_new_address(Some("alice"), None).unwrap();
+
+    for _ in 0..10 {
+        node.client
+            .generate_to_address(160, &alice_address)
+            .unwrap();
+    }
+
+    let tip_hash = node.client.get_best_block_hash().unwrap();
+    let tip_height = node.client.get_block_header_info(&tip_hash).unwrap().height;
+
+    let tip_header = node.client.get_block_header(&tip_hash).unwrap();
+
+    let header_config = HeaderConfig {
+        max_length: 2000,
+        max_time_increase: 8 * 60 * 60,
+        trusted_height: tip_height as u32,
+        retarget_interval: 2016,
+        target_spacing: 10 * 60,
+        target_timespan: 2016 * (10 * 60),
+        max_target: 0x1d00ffff,
+        retargeting: true,
+        min_difficulty_blocks: false,
+        trusted_header: Adapter::from(tip_header),
+    };
+    app.execute(
+        owner.clone(),
+        bitcoin_bridge_addr.clone(),
+        &msg::ExecuteMsg::UpdateHeaderConfig {
+            config: header_config,
+        },
+        &[],
+    )
+    .unwrap();
+
+    let mut headers = Vec::with_capacity(11);
+    for _ in 0..250 {
+        node.client.generate_to_address(1, &alice_address).unwrap();
+
+        let tip_hash = node.client.get_best_block_hash().unwrap();
+        let tip_header = node.client.get_block_header(&tip_hash).unwrap();
+        let tip_height_info = node.client.get_block_header_info(&tip_hash).unwrap();
+        let tip_height = tip_height_info.height;
+
+        headers.push(WrappedHeader::from_header(&tip_header, tip_height as u32));
+    }
+
+    let res = app
+        .execute(
+            owner.clone(),
+            bitcoin_bridge_addr.clone(),
+            &msg::ExecuteMsg::RelayHeaders {
+                headers: headers.clone(),
+            },
+            &[],
+        )
+        .unwrap();
+
+    println!("gas used: {:?}", res.gas_info.gas_used);
+
+    let header_height: i32 = app
+        .query(bitcoin_bridge_addr.clone(), &msg::QueryMsg::HeaderHeight {})
+        .unwrap();
+
+    assert_eq!(header_height, 1850);
+}
+
+#[test]
 fn reorg() {
     // Set up app
 
